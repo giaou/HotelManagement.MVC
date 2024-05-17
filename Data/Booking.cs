727e86808d9f3ef46327d7e1aac330dff7fd80e6
Giao U <lesliegiao.u@gmail.com>
2024-05-17 14:22:37 -0500

    Added booking.cs model

---
+++ Data/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelManagement.MVC.Data;
+
+public partial class Booking
+{
+    public int Id { get; set; }
+
+    public int? CustomerId { get; set; }
+
+    public int? RoomId { get; set; }
+
+    public virtual Customer? Customer { get; set; }
+
+    public virtual Room? Room { get; set; }
+}
