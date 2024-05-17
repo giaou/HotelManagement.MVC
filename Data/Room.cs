12326371d771eef6d0cd5f4a229c583b3ef459a2
Giao U <lesliegiao.u@gmail.com>
2024-05-17 13:43:37 -0500

    Modified room.cs

--- Data/Room.cs
+++ Data/Room.cs
@@ -8,9 +8,7 @@ public partial class Room
 {
     public int Id { get; set; }
 
-    [StringLength(5,ErrorMessage ="Max 5 Characters")]
     public string RoomNumber { get; set; } = null!;
 
-    [Range(0,1, ErrorMessage ="Can be either 0 for unbooked or 1 for booked")]
     public int RoomStatus { get; set; }
 }
