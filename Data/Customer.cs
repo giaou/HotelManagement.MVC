5f572eb34edc6c49a27bce3850b6fabc172a9476
Giao U <lesliegiao.u@gmail.com>
2024-05-18 14:06:55 -0500

    Modified Customer.cs

--- Data/Customer.cs
+++ Data/Customer.cs
@@ -19,7 +19,7 @@ public partial class Customer
 
     public string CardNumber { get; set; } = null!;
 
-    public DateOnly DateOfBirth { get; set; }
+    public DateTime DateOfBirth { get; set; }
 
     public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 }
